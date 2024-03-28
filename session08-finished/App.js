cecc9ab84b08f1352d86c279ac3eb8d80f49bf96
Pabitapun23 <pabita.pun23@gmail.com>
2024-03-28 15:11:43 -0400

    all the code done

---
+++ session08-finished/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import SignInScreen from './screens/SignInScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SignUpScreen from './screens/SignUpScreen';
+
+//to obtain instance of navigation stack
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (    
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          component={SignUpScreen} 
+          name="SignUp" 
+          options={{
+            headerStyle:{backgroundColor:"#DC267F"}, 
+            headerTintColor:"#fff"}}></Tab.Screen>
+        <Tab.Screen component={SignInScreen} name="SignIn" options={{headerStyle:{backgroundColor:"#E69F00"}, headerTintColor:"#fff"}}></Tab.Screen>        
+        <Tab.Screen component={ProfileScreen} name="Profile" options={{headerStyle:{backgroundColor:"#648FFF"}, headerTintColor:"#fff"}}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
